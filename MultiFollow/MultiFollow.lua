be0dd4a8ca91c07fa4e8d8606295672d12185cbf
V3XBANE <46516092+Vexbane@users.noreply.github.com>
2025-05-04 10:18:01 -0400

    Update MultiFollow.lua

--- MultiFollow/MultiFollow.lua
+++ MultiFollow/MultiFollow.lua
@@ -1,21 +1,55 @@
 --[[ 
-  MultiFollow addon was created by: Bane & Killer from [V3X] Community (discord.gg/XEU75MR), name given by vB1OS
-  This has been tested to work with WoW version 3.3.5a but may work with others
-  Important Info:  This will function fully ONLY with 3 or more players in party or raid
-  Type Commands in:  Party, Raid or Whisper
-  Available Commands:  
--  mf (everyone will follow in raid or party)
--  mount (will force everyone to mount and follow that target, must be standing still or it will follow first - then must send 'mount' command again)
--  mfstop (forces everyone following their target by targeting another random player in raid or party.  Blizzard in WoTLK broke the follow self or other commands that were used to stop someone from following, thus - this work around)
+  ## Author: Bane from [V3X] Gaming discord.gg/XEU75MR | Credits: to Killer for assisting + name of addon: vB1OS (see "readme.txt" for more info)
 ]]
 
 local MultiFollowFrame = CreateFrame("Frame")
 
+-- Variables to track status
+local isEnabled = true
+local leaderOnly = false
+
+-- Slash Commands Setup
 SLASH_MULTIFOLLOW1 = "/mf"
-SlashCmdList["MULTIFOLLOW"] = function()
-  print("MultiFollow loaded. Type 'mf' to follow, 'mount' to mount and follow, 'mfstop' to break follow and dismount. Works with 3 or more people in party or raid.")
+SlashCmdList["MULTIFOLLOW"] = function(msg)
+  local command = string.lower(msg or "")
+
+  if command == "" then
+    print("|cFFFFFF00Type in Party, Raid or Whisper Chat:|r")
+    print("|cFFFFFFFFmf|r |cFFFFFF00(everyone with addon follows you)|r")
+    print("|cFFFFFFFFmount|r |cFFFFFF00(everyone with addon will follow + mount) (type 2x)|r")
+    print("|cFFFFFFFFmfstop|r |cFFFFFF00(everyone with addon stops following you)|r")
+    print("|cFFFFFF00Type in-game /slash commands:|r")
+    print("|cFFFFFFFF/mf|r |cFFFFFF00(prints this message and shows status)|r")
+    print("|cFFFFFFFF/mf enable|r |cFFFFFF00(enables MultiFollow addon)|r")
+    print("|cFFFFFFFF/mf disable|r |cFFFFFF00(disables MultiFollow addon)|r")
+    print("|cFFFFFFFF/mf leader on|r |cFFFFFF00(only party or raid leader can command you)|r")
+    print("|cFFFFFFFF/mf leader off|r |cFFFFFF00(anyone can command you)|r")
+    print("|cFFFFFF00Current Status:|r")
+    print("|cFFFFFF00Addon:|r |cFFFFFFFF" .. (isEnabled and "ENABLED" or "DISABLED") .. "|r")
+    print("|cFFFFFF00Leader Only:|r |cFFFFFFFF" .. (leaderOnly and "ON" or "OFF") .. "|r")
+
+  elseif command == "enable" then
+    isEnabled = true
+    print("|cFFFFFF00MultiFollow addon ENABLED.|r")
+
+  elseif command == "disable" then
+    isEnabled = false
+    print("|cFFFFFF00MultiFollow addon DISABLED.|r")
+
+  elseif command == "leader on" then
+    leaderOnly = true
+    print("|cFFFFFF00Leader-only mode ENABLED. Only raid/party leaders can command you.|r")
+
+  elseif command == "leader off" then
+    leaderOnly = false
+    print("|cFFFFFF00Leader-only mode DISABLED. Anyone in party/raid/whisper can command you.|r")
+
+  else
+    print("|cFFFFFF00Unknown /mf command. Type /mf for help.|r")
+  end
 end
 
+-- Helper: Get all group member names
 local function GetGroupMemberNames()
   local groupMembers = {}
   local selfName = UnitName("player")
@@ -42,23 +76,22 @@ local function GetGroupMemberNames()
   return groupMembers
 end
 
+-- Helper: Mount best available mount
 local function MountBestAvailable()
   local numMounts = GetNumCompanions("MOUNT")
   local isFlyable = IsFlyableArea()
 
-  -- Try to find a flying mount first based on name assumption
   for i = 1, numMounts do
     local creatureID, name, spellID, icon, isSummoned = GetCompanionInfo("MOUNT", i)
     if not isSummoned and isFlyable and (
-        name:lower():find("gryphon") or name:lower():find("wind") or
-        name:lower():find("nether") or name:lower():find("drake") or
-        name:lower():find("ray") or name:lower():find("proto")) then
+      name:lower():find("gryphon") or name:lower():find("wind") or
+      name:lower():find("nether") or name:lower():find("drake") or
+      name:lower():find("ray") or name:lower():find("proto")) then
       CallCompanion("MOUNT", i)
       return
     end
   end
 
-  -- Fallback: use first unsummoned mount
   for i = 1, numMounts do
     local creatureID, name, spellID, icon, isSummoned = GetCompanionInfo("MOUNT", i)
     if not isSummoned then
@@ -68,10 +101,48 @@ local function MountBestAvailable()
   end
 end
 
+-- Helper: Check if sender is leader
+local function IsSenderLeader(senderName)
+  -- Check Party
+  if GetNumPartyMembers() > 0 and GetNumRaidMembers() == 0 then
+    for i = 1, GetNumPartyMembers() do
+      local unit = "party" .. i
+      if UnitName(unit) == senderName and UnitIsPartyLeader(unit) then
+        return true
+      end
+    end
+    if senderName == UnitName("player") and IsPartyLeader() then
+      return true
+    end
+  end
+
+  -- Check Raid
+  if GetNumRaidMembers() > 0 then
+    for i = 1, GetNumRaidMembers() do
+      local unit = "raid" .. i
+      if UnitName(unit) == senderName and UnitIsPartyLeader(unit) then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+-- Main chat message handler
 local function handleChatMsg(msg, player)
+  if not isEnabled then return end
+
   local msgUpper = string.upper(msg)
   local senderName = player
   local selfName = UnitName("player")
+
+  -- Leader-only enforcement
+  if leaderOnly then
+    if not IsSenderLeader(senderName) then
+      return
+    end
+  end
 
   if msgUpper == 'MF' and senderName ~= selfName then
     FollowUnit(senderName)
@@ -96,6 +167,7 @@ local function handleChatMsg(msg, player)
   end
 end
 
+-- Event Handler
 local function EventHandler(self, event, arg1, arg2)
   if event == 'CHAT_MSG_PARTY' or event == 'CHAT_MSG_PARTY_LEADER'
   or event == 'CHAT_MSG_RAID' or event == 'CHAT_MSG_RAID_LEADER'
@@ -104,6 +176,7 @@ local function EventHandler(self, event, arg1, arg2)
   end
 end
 
+-- Login Setup
 local function LoginEvent(self, event)
   MultiFollowFrame:UnregisterEvent("PLAYER_LOGIN")
   MultiFollowFrame:SetScript("OnEvent", EventHandler)
@@ -112,6 +185,10 @@ local function LoginEvent(self, event)
   MultiFollowFrame:RegisterEvent("CHAT_MSG_RAID")
   MultiFollowFrame:RegisterEvent("CHAT_MSG_RAID_LEADER")
   MultiFollowFrame:RegisterEvent("CHAT_MSG_WHISPER")
+
+  -- Pretty colored load message split across 2 lines
+  print("|cFFFFFF00 ++++++++++++++++++|r |cFF3399FFMultiFollow:|r |cFFFFFF00++ Loaded ++|r |cFF3399FF/mf|r")
+  print("|cFFFFFF00 ++++++++++++++++++|r |cFF3399FFDiscord.gg/|r|cFFFFFFFFXEU75MR|r |cFFFFFF00(Cap Sensitive)|r")
 end
 
 MultiFollowFrame:SetScript("OnEvent", LoginEvent)
